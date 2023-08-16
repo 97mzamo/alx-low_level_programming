49ff9ac8e5a1bf175e55e74e3068629ea81c795b
97mzamo <smothinifedrick97@gmail.com>
2023-08-16 05:54:10 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	_putchar('\n');
+}
